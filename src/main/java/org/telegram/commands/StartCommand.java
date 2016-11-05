d97bf37ffd98c924873b20a73986fe92a58515ca
michal kostyal <KostyaM@cpwplc.com>
2016-11-05 12:14:25 +0100

    Revert "I have added buttons"
    This reverts commit bfafe944f454436eb1f27c1c55860d42a43b9d5d.

--- src/main/java/org/telegram/commands/StartCommand.java
+++ src/main/java/org/telegram/commands/StartCommand.java
@@ -1,23 +1,12 @@
 package org.telegram.commands;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.json.JSONObject;
 import org.telegram.database.DatabaseManager;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Chat;
 import org.telegram.telegrambots.api.objects.User;
-import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.bots.commands.BotCommand;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.exceptions.TelegramApiValidationException;
 import org.telegram.telegrambots.logging.BotLogger;
 
 /**
@@ -36,51 +25,6 @@ public class StartCommand extends BotCommand {
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
         
-        InlineKeyboardMarkup replyMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList();
-        List<InlineKeyboardButton> firstRow = new ArrayList();
-        List<InlineKeyboardButton> secondRow = new ArrayList();
-        List<InlineKeyboardButton> thirdRow = new ArrayList();
-        
-    // FIRST ROW
-        InlineKeyboardButton gameButton = new InlineKeyboardButton();
-        gameButton.setText("Math Challenge");
-        gameButton.setCallbackData("1");
-        firstRow.add(gameButton);
-        
-    // SECOND ROW
-        InlineKeyboardButton registerButton = new InlineKeyboardButton();
-        registerButton.setText("Register");
-        registerButton.setCallbackData("1");
-        secondRow.add(registerButton);
-        
-        InlineKeyboardButton checkCreditButton = new InlineKeyboardButton();
-        checkCreditButton.setText("Check Credit");
-        checkCreditButton.setCallbackData("1");
-        secondRow.add(checkCreditButton);
-        
-        InlineKeyboardButton startChallengeButton = new InlineKeyboardButton();
-        startChallengeButton.setText("Start Challenge");
-        startChallengeButton.setCallbackData("1");
-        secondRow.add(startChallengeButton);
-        
-    // THIRD ROW
-        InlineKeyboardButton spendCreditButton = new InlineKeyboardButton();
-        spendCreditButton.setText("Spend credit");
-        spendCreditButton.setCallbackData("1");
-        secondRow.add(spendCreditButton);
-    
-        InlineKeyboardButton viewHighestScoreButton = new InlineKeyboardButton();
-        viewHighestScoreButton.setText("View highest score");
-        viewHighestScoreButton.setCallbackData("1");
-        secondRow.add(viewHighestScoreButton);
-    
-        keyboard.add(firstRow);
-        keyboard.add(secondRow);
-        keyboard.add(thirdRow);
-        replyMarkup.setKeyboard(keyboard);
-     
-        
         StringBuilder messageBuilder = new StringBuilder();
 
         String userName = user.getFirstName() + " " + user.getLastName();
@@ -91,8 +35,6 @@ public class StartCommand extends BotCommand {
         SendMessage answer = new SendMessage();
         answer.setChatId(chat.getId().toString());
         answer.setText(messageBuilder.toString());
-        
-        answer.setReplyMarkup(replyMarkup);
 
         try {
             absSender.sendMessage(answer);
